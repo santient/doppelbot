897037f9f02319e50dd8ed5dcc9ef421ff3aa486
Santiago Benoit <santib27@gmail.com>
2019-07-04 22:13:55 -0400

    move parsing code to dataset.py

--- doppelbot-lstm/train.py
+++ doppelbot-lstm/train.py
@@ -1,35 +1,9 @@
-import re
-import unicodedata
-
-
-def unicode_to_ascii(s):
-    # convert to ascii
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
-
-
-def normalize_string(s):
-    # discard unnecessary characters
-    s = unicode_to_ascii(s.strip())
-    s = re.sub(r"([.!?])", r"\1", s)
-    s = re.sub(r"[^a-zA-Z.!?,'""0-9]+", r" ", s)
-    return s
-
-
-def read_lines(path="dialogues_text.txt"):
-    print("Reading Lines...")
-
-    # read the file
-    try:
-        src = open(path, 'r', encoding='utf-8').read().split('\n')
-    except:
-        raise FileNotFoundError
-    src = ["<SOM>" + normalize_string(s) + "<EOM>" for s in src]
-    return src
+# imports here
 
 
 def train():
     # training code here
     pass
+
+if __name__ == "__main__":
+    train()
