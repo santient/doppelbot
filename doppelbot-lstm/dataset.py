3265ef39810eda9f7cf2a5e10ae62581ecd0132a
Santiago Benoit <santib27@gmail.com>
2019-07-11 01:52:46 -0400

    placeholder dataset

--- doppelbot-lstm/dataset.py
+++ doppelbot-lstm/dataset.py
@@ -1,6 +1,19 @@
 import re
 import unicodedata
+import torch
+from torch.utils.data import Dataset
 
+
+class ConversationDataset(Dataset):
+    def __init__(self):
+        super()
+        self.data = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
 
 def unicode_to_ascii(s):
     # convert to ascii
@@ -9,14 +22,12 @@ def unicode_to_ascii(s):
         if unicodedata.category(c) != 'Mn'
     )
 
-
 def normalize_string(s):
     # discard unnecessary characters
     s = unicode_to_ascii(s.strip())
     s = re.sub(r"([.!?])", r"\1", s)
     s = re.sub(r"[^a-zA-Z.!?,'""0-9]+", r" ", s)
     return s
-
 
 def read_lines(path="dialogues_text.txt"):
     print("Reading Lines...")
